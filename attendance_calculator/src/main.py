b610b52ae2dce56e9b3bbf282b4376bb797c42b6
AbhiBD <abhineet.banerjee01@gmail.com>
2025-08-26 13:14:55 +0530

    Second Draft

--- attendance_calculator/src/main.py
+++ attendance_calculator/src/main.py
@@ -1,4 +1,4 @@
-from utils import get_classes_needed, get_current_attendance
+from utils import get_classes_needed, get_current_attendance, get_required_classes
 
 def main():
     subject_name = input("Enter the subject name: ")
@@ -8,8 +8,10 @@ def main():
 
     current_attendance = get_current_attendance(attended_classes, total_classes)
     classes_needed = get_classes_needed(total_classes, attended_classes, required_percentage)
+    required_classes = get_required_classes(total_classes, required_percentage)
 
     print("\nSubject: {}".format(subject_name))
+    print("Number of classes required to achieve {}% attendance : {}".format(required_percentage,required_classes))
     print("Current Attendance: {:.2f}%".format(current_attendance))
     print("Classes needed to maintain {}% attendance: {}".format(required_percentage, classes_needed))
 
--- attendance_calculator/src/utils.py
+++ attendance_calculator/src/utils.py
@@ -1,9 +1,17 @@
+import math
+
 def get_classes_needed(total_classes, attended_classes, required_percentage):
     required_classes = (required_percentage / 100) * total_classes
     classes_needed = required_classes - attended_classes
+    classes_needed = int(math.ceil(classes_needed))
     return max(0, classes_needed)
 
 def get_current_attendance(attended_classes, total_classes):
     if total_classes == 0:
         return 0
     return (attended_classes / total_classes) * 100
+
+def get_required_classes(total_classes, required_percentage):
+    required_classes = (required_percentage / 100) * total_classes
+    required_classes = int(math.ceil(required_classes))
+    return max(0, required_classes)
